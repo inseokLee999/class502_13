5f993db05f116f0a9e72b61cebd9a210ccf77246
inseokLee999 <syt06137@naver.com>
2024-04-12 14:05:22 +0900

    오전 수업

---
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex01.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args){
+        int num1;//변수 선언
+        num1=10;//초기화 : 최초로 값을 대입하는 것
+        int num2=10;//선언과 동시에 초기화
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        int a=100;
+    }
+}
---
+++ 3. 응용 SW 기초 기술활용/source/day01_2/src/exam01/Ex02.java
@@ -0,0 +1,10 @@
+package exam01;
+
+public class Ex02 {
+    public  static void main(String[] args){
+        int num1=1000000;
+        //byte num2=num1;
+
+        double num3=1000000000000.1234;
+    }
+}
