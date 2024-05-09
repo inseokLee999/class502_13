053b0ae43f44f3f7375e623a911f31a2341099ca
inseokLee999 <syt06137@naver.com>
2024-05-09 17:01:16 +0900

    0509 수업 완료

---
+++ 4. 프로그래밍 언어활용/day18/src/exam01/Ex02.java
@@ -0,0 +1,20 @@
+package exam01;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        LocalDate endDate = LocalDate.of(2024,9,30);
+
+        Period period = Period.between(today,endDate);
+        System.out.println(period);
+
+        int month = period.getMonths();
+        int day = period.getDays();
+        System.out.printf("남은 수업은 :  %d 개월, %d일\n",month,day);
+
+
+    }
+}
