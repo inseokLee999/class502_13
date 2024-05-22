4dcce9874d0e73a17c8c7de020f7fbfe797733ae
inseokLee999 <syt06137@naver.com>
2024-05-22 17:24:41 +0900

    0522 수업 완료

--- 7. SQL 응용/exam02/src/main/java/member/Member.java
+++ 7. SQL 응용/exam02/src/main/java/member/Member.java
@@ -3,13 +3,19 @@ package member;
 import lombok.*;
 
 import java.time.LocalDateTime;
-/*@Getter @Setter @ToString*/
+/*@Getter @Setter @ToString @EqualsAndHashCode*/
 @Data
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor//기본생성자
+//@NoArgsConstructor(access = AccessLevel.PRIVATE)//기본생성자
+@NoArgsConstructor
+@AllArgsConstructor//모든 매개 변수가 생성자로 들어감
+@RequiredArgsConstructor
 public class Member {
+    @NonNull
     private String userId;
+
+    @NonNull
     private String userNm;
     private String email;
+    @ToString.Exclude
     private LocalDateTime regDt;
 }
