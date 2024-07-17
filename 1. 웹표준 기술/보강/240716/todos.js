e85d820b5679c0e93003b0ade4f63339d60f93a7
inseokLee999 <syt06137@naver.com>
2024-07-17 18:38:16 +0900

    0717 js 수업완료

--- 1. 웹표준 기술/보강/240716/todos.js
+++ 1. 웹표준 기술/보강/240716/todos.js
@@ -1,7 +1,28 @@
 const todos = {
-    add(){
+    // 스케줄 추가
+    add(todo){
+        // .?? 값이 있는지 없는지 nullish 널 병합 연산자
+        todo = todo ?? "";
+        todo = todo.trim()
+        if(!todo){
+            throw new Error("스케줄을 입력하세요");
+        }
+        const button = document.createElement("button");
+        button.append("삭제");
 
+        const liEl = document.createElement("li");
+        liEl.append(todo,button);
+
+        const schedulesEl = document.getElementById("schedules");
+        schedulesEl.append(liEl)
+        // 스케줄 삭제
+        button.addEventListener("click",function(){
+            if(confirm("정말 삭제 하시겠습니까?")){
+                liEl.remove();
+            }
+        });
     },
+    // 스케줄 제거
     remove(){
         
     }
@@ -10,9 +31,14 @@ window.addEventListener("DOMContentLoaded",function(){
     frmRegist.addEventListener("submit",function(e){
         // this = event가 발생한 가장 중요한 것 : frmRegist
         e.preventDefault();
-        const todo = this.todo.value.trim()
-        const liEl = document.createElement("li");
-        liEl.appendChild(document.createTextNode(todo));
-        document.getElementById("schedules").appendChild(liEl);
+        const todoEl = this.todo;
+        try {
+            todos.add(todoEl.value); // 스케줄 등록
+      
+            todoEl.value = "";
+          } catch (e) {
+            alert(e.message);
+          }
+          todoEl.focus();
     });
 });
